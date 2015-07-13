b9d31cdf45ecb20302e13ed3caebeda3fa286de9
Kha & Garrett <student@epicodus.com>
2015-07-13 13:11:36 -0700

    Ongoing work for choice voting

--- app/controllers/rathers_controller.rb
+++ app/controllers/rathers_controller.rb
@@ -17,10 +17,31 @@ class RathersController < ApplicationController
     end
   end
 
+  def update
+    rather = Rather.find(params[:id])
+
+    binding.pry
+
+    if rather_params[:choice_one_text]
+      choice_one_votes = rather.choice_one_votes
+      rather.update(choice_one_votes: choice_one_votes + 1);
+    else
+      choice_two_votes = rather.choice_two_votes
+      rather.update(choice_two_votes: choice_two_votes + 1);
+    end
+
+    rather.save
+
+    respond_to do |format|
+      format.html { redirect_to :back }
+      format.js
+    end
+  end
+
   private
 
   def rather_params
-    params.require(:rather).permit(:choice_one_text, :choice_two_text)
+    params.require(:rather).permit(:choice_one_text, :choice_two_text, :choice_picked)
   end
 
 end
